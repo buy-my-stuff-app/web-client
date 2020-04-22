a0a0064e3cfc293f34725c33c0e6500a1798557e
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-04-22 17:44:46 +0200

    Add TopNav component

--- src/components/UI/AppPage/AppPage.styles.jsx
+++ src/components/UI/AppPage/AppPage.styles.jsx
@@ -3,6 +3,7 @@ import styled, {css} from 'styled-components';
 export const Styles = styled.div`
   .AppPage {
     &__main {
+      display: flex;
       flex: 1;
       flex-direction: column;
       overflow-y: auto;
--- src/pages/MainPage/layout/MainPageMobile/MainPageMobile.jsx
+++ src/pages/MainPage/layout/MainPageMobile/MainPageMobile.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import {Styles} from './MainPageMobile.styles';
 import Theme from '../../../../themes/Theme';
 import AppPage from '../../../../components/UI/AppPage/AppPage';
+import TopNav from '../../../../components/UI/TopNav/TopNav';
 
 const MainPageMobile = ({theme}) => {
   return (
     <Styles theme={theme}>
-      <AppPage>
+      <AppPage TopNav={<TopNav />}>
         <p>MainPageMobile</p>
       </AppPage>
     </Styles>
