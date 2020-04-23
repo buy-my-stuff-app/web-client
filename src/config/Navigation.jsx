b3fba02036f17950a2209e2ea34ac705fe068585
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-04-23 18:16:38 +0200

    Remove comments on Navigation component

--- src/config/Navigation.jsx
+++ src/config/Navigation.jsx
@@ -1,35 +1,34 @@
-import React, { useMemo } from "react";
-import PropTypes from "prop-types";
-import { Switch, Route, Redirect } from "react-router-dom";
-import MainPage from "../pages/MainPage/MainPage";
-import Login from "../pages/Login/Login";
-import Signup from "../pages/Signup/Signup";
-import RememberPassword from "../pages/RememberPassword/RememberPassword";
+import React, {useMemo} from 'react';
+import PropTypes from 'prop-types';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import MainPage from '../pages/MainPage/MainPage';
+import Login from '../pages/Login/Login';
+import Signup from '../pages/Signup/Signup';
+import RememberPassword from '../pages/RememberPassword/RememberPassword';
 
 const AuthRouter = (
   <Switch>
-    <Route exact path="/" component={MainPage} />
-    <Route path="/:product_id" component={MainPage} /> {/* Product detail */}
-    <Route render={() => <Redirect to="/" />} />
+    <Route exact path='/' component={MainPage} />
+    <Route path='/:product_id' component={MainPage} />
+    <Route render={() => <Redirect to='/' />} />
   </Switch>
 );
 
 const GuestRouter = (
   <Switch>
-    <Route exact path="/" component={MainPage} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/signup" component={Signup} />
-    <Route exact path="/remember-password" component={RememberPassword} />
-    <Route exact path="/cart" component={MainPage} /> {/* Cart */}
-    <Route exact path="/checkout" component={MainPage} /> {/* Checkout */}
-    <Route exact path="/checkout-completed" component={MainPage} />{" "}
-    {/* Checkout completed */}
-    <Route path="/:product_id" component={MainPage} /> {/* Product detail */}
-    <Route render={() => <Redirect to="/" />} />
+    <Route exact path='/' component={MainPage} />
+    <Route exact path='/login' component={Login} />
+    <Route exact path='/signup' component={Signup} />
+    <Route exact path='/remember-password' component={RememberPassword} />
+    <Route path='/product/:product_id' component={MainPage} />
+    <Route exact path='/cart' component={MainPage} />
+    <Route exact path='/checkout' component={MainPage} />
+    <Route exact path='/checkout-completed' component={MainPage} />
+    <Route render={() => <Redirect to='/' />} />
   </Switch>
 );
 
-const Navigation = ({ userLoggedId }) => {
+const Navigation = ({userLoggedId}) => {
   const isLogged = useMemo(() => !!userLoggedId, [userLoggedId]);
 
   if (isLogged) {
@@ -40,7 +39,7 @@ const Navigation = ({ userLoggedId }) => {
 };
 
 Navigation.propTypes = {
-  userLoggedId: PropTypes.string
+  userLoggedId: PropTypes.string,
 };
 
 export default Navigation;
